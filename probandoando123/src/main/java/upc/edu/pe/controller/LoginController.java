4c004b2e133c236b5e3af3020ed80edc897c6470
Anthamy22 <anthamybarreraalarcon@gmail.com>
2021-06-29 09:41:37 -0500

    UPDTE
    UPDT NAVBARS
    
    Co-Authored-By: RenzoRG18 <51940916+RenzoRG18@users.noreply.github.com>

--- probandoando123/src/main/java/upc/edu/pe/controller/LoginController.java
+++ probandoando123/src/main/java/upc/edu/pe/controller/LoginController.java
@@ -1,20 +1,37 @@
 package upc.edu.pe.controller;
 
+import java.security.Principal;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 public class LoginController {
 	
-	@GetMapping("/login")
-	public String login() {
+
+	@GetMapping(value = { "/login" })
+	public String login(@RequestParam(value = "error", required = false) String error,
+			@RequestParam(value = "logout", required = false) String logout, Model model, Principal principal,
+			RedirectAttributes flash) {
+
+		if (principal != null) {
+
+			flash.addFlashAttribute("Info", "Ya ha iniciado sesion anteriormente");
+			return "redirect:/welcome/bienvenido";
+		}
+
+		if (error != null) {
+			
+			model.addAttribute("error", "Error en el login:Nombre de usuario o contraseña incorrecto");
+		}
+		if (logout != null) {
+			model.addAttribute("success", "Ha cerrado sesion con exito");
+		}
+
 		return "login";
-	}
-	
-	
-	@GetMapping("/")
-	public String irlogin() {
-		return "redirect:/login";
 	}
 	
 }
